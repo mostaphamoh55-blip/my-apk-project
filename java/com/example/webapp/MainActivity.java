159068c11f21ca21aa05c4f7bd8b895fffcaf0d2
mostaphamoh55-blip <mostaphamoh55@gmail.com>
2026-09-19 19:28:52 +0600

    Create MainActivity to display web content
    Initialize MainActivity with a WebView to load Google.

---
+++ java/com/example/webapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.webapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com");
+    }
+}
